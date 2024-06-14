fc23b18e8c73c972cfdfbe1474e937d1f5f66ec5
Ivan Danilyuk <113250722+ArdelSmith@users.noreply.github.com>
2024-06-14 21:27:47 +0500

    smth

--- requests.py
+++ requests.py
@@ -2,13 +2,23 @@ import requests
 import json
 
 
-def api_get(id):
+#вернут статус на то могут ли они это забронировать
+def check_hotel_booking_available():
     r = requests.get(f'http://api:80/get/{id}')
     response = json.loads(r.text)
     return response
 
-
+#обращаемся чтобы они забронировали
 def api_add(id):
     r = requests.post(f'http://api:80/add/{id}')
     response = json.loads(r.text)
     return response
+
+#просим забронировать транспорт какой-то
+def transport_book():
+    return
+
+#имитация отправки mock запроса
+def send_mock_cash():
+    r = requests
+    return
