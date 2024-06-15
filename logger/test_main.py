4a9016d4d56d7e672f6da34c020d58ed95fe2e9b
Gregg <arkhipov.zena@gmail.com>
2024-06-15 20:35:47 +0500

    fix: renaming

--- logger/test_main.py
+++ logger/test_main.py
@@ -1,9 +1,10 @@
 from logger import log_message
 
-def main():
+
+def test_main():
     # Example usage of the logger
     log_message("Файл успешно сохранен")  # first call
     log_message("Файл не удалось сохранить")  # second call
 
-if __name__ == "__main__":
-    main()
+
+test_main()
