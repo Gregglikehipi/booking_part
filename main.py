39a786adcdda337eaf5d2f82fbed566307ff5f2f
Gregg <arkhipov.zena@gmail.com>
2024-06-17 15:22:41 +0500

    feat: small change in api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Union, List, Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
-from logic import confirm_all
+from logic import confirm_all, confirm_transport, confirm_hotel
 
 
 class Hotel(BaseModel):
@@ -24,11 +24,11 @@ class TransportList(BaseModel):
 
 
 class HotelReq(Hotel):
-    status: str
+    status: bool
 
 
 class TransportReq(Transport):
-    status: str
+    status: bool
 
 
 class HotelReqList(BaseModel):
@@ -52,7 +52,19 @@ app = FastAPI()
 @app.get("/api/ui_root")
 def ui_root(ui: UI):
     data = dict(ui)
-    data_req = confirm_all(data)
+    data_req = data
+    hotel_bool = False
+    transport_bool = False
+    if "hotels" in data:
+        hotel_bool = True
+    if "order_id" in data:
+        transport_bool = True
+    if hotel_bool and transport_bool:
+        data_req = confirm_all(data)
+    if not hotel_bool and transport_bool:
+        data_req = confirm_transport(data)
+    if hotel_bool and not transport_bool:
+        data_req = confirm_hotel(data)
     return data_req
 
 
